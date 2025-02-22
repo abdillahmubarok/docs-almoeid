6d6b75dc3c52c6197a762a0d9e46c6d207c31bf1
Wildan Nursahidan <edpwildan@gmail.com>
2025-02-22 21:39:04 +0700

    starter themes

---
+++ README.md
@@ -0,0 +1,71 @@
+# DocuBook
+
+**DocuBook** is a documentation web project designed to provide a simple and user-friendly interface for accessing various types of documentation. This site is crafted for developers and teams who need quick access to references, guides, and essential documents.
+
+> **Note**: This application is a fork of [AriaDocs](https://github.com/nisabmohd/Aria-Docs), created by [Nisab Mohd](https://github.com/nisabmohd). DocuBook provides an alternative to the documentation solution found on [Mintlify](https://mintlify.com/), utilizing `.mdx` (Markdown + JSX) for content creation and management.
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/import/project?template=https://github.com/mywildancloud/docubook)
+
+## Features
+
+- **Easy Navigation**: Simple layout for quick navigation between pages.
+- **Quick Search**: Easily find documentation using a search function.
+- **Responsive Theme**: Responsive design optimized for devices ranging from desktops to mobile.
+- **Markdown Content**: Support for markdown-based documents.
+- **SEO Friendly**: Optimized structure for search visibility, enhancing accessibility on search engines.
+
+## Installation
+
+```bash
+npx @docubook/create@latest
+```
+
+#### command output
+
+```bash
+? Enter a name for your project directory: (docubook)
+
+Creating a new Docubook project in /path/your/docubook from the main branch...
+✔ Docubook project successfully created in /path/your/docubook!
+
+Next steps:
+1. Navigate to your project directory:
+   cd docubook
+2. Install dependencies:
+   npm install
+3. Start the development server:
+   npm run dev
+```
+
+## Update
+### How to Update DocuBook?
+- **Open a New Terminal**: Please open a new terminal on the desktop that has DocuBook installed.
+- **Move Directory**: for example, if the directory name is docubook, then write `cd docubook` and press enter.
+
+```bash
+npx @docubook/update@latest
+```
+
+#### command output
+
+```bash
+📂 Updating Docubook project in /Users/wildan/Public/docubook...
+
+ℹ ⚡ Skipped public
+ℹ ⚡ Skipped contents
+ℹ ⚡ Skipped app/page.tsx
+ℹ ⚡ Skipped docu.json
+ℹ ⚡ Skipped CHANGELOG.md
+✨ Replacing styles folder...
+✨ Replaced all CSS files in styles folder
+✔ ✅ Docubook v1.4.2 successfully updated in /Users/wildan/Public/docubook!
+
+🎯 Next steps:
+1. Verify your changes in the current directory.
+2. Run the install script to check for package updates:
+   npm install
+3. Run the development server:
+   npm run dev
+```
+
+Access the app on => http://localhost:3000
---
+++ components/navbar.tsx
@@ -0,0 +1,99 @@
+import { ModeToggle } from "@/components/theme-toggle";
+import { ArrowUpRight } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import { buttonVariants } from "./ui/button";
+import Search from "./search";
+import Anchor from "./anchor";
+import { SheetLeftbar } from "./leftbar";
+import { SheetClose } from "@/components/ui/sheet";
+import docuConfig from "@/docu.json"; // Import JSON
+
+export function Navbar() {
+  const { social } = docuConfig; // Extract navbar and social from JSON
+
+  return (
+    <nav className="w-full border-b h-16 sticky top-0 z-50 bg-background">
+      <div className="sm:container mx-auto w-[95vw] h-full flex items-center justify-between md:gap-2">
+        <div className="flex items-center gap-5">
+          <SheetLeftbar />
+          <div className="flex items-center gap-6">
+            <div className="sm:flex hidden">
+              <Logo />
+            </div>
+            <div className="lg:flex hidden items-center gap-4 text-sm font-medium text-muted-foreground">
+              <NavMenu />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-3">
+          <div className="flex items-center gap-2">
+            <Search />
+            <div className="flex ml-2.5 sm:ml-0 gap-2">
+              {social.map((item) => {
+                const Icon = require("lucide-react")[item.iconName]; // Dynamically load icon
+                return (
+                  <Link
+                    key={item.name}
+                    href={item.url}
+                    target="_blank"
+                    className={buttonVariants({ variant: "ghost", size: "icon" })}
+                  >
+                    <Icon className="h-[1.1rem] w-[1.1rem]" />
+                  </Link>
+                );
+              })}
+              <ModeToggle />
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export function Logo() {
+  const { navbar } = docuConfig; // Extract navbar from JSON
+
+  return (
+    <Link href="/" className="flex items-center gap-2.5">
+      <Image src={navbar.logo.src} alt={navbar.logo.alt} width="24" height="24" />
+      <h2 className="text-md font-bold font-code">{navbar.logoText}</h2>
+    </Link>
+  );
+}
+
+export function NavMenu({ isSheet = false }) {
+  const { navbar } = docuConfig; // Extract navbar from JSON
+
+  return (
+    <>
+      {navbar.menu.map((item) => {
+        const isExternal = item.href.startsWith("http");
+
+        const Comp = (
+          <Anchor
+            key={item.title + item.href}
+            activeClassName="!text-primary md:font-semibold font-medium"
+            absolute
+            className="flex items-center gap-1 dark:text-stone-300/85 text-stone-800"
+            href={item.href}
+            target={isExternal ? "_blank" : undefined}
+            rel={isExternal ? "noopener noreferrer" : undefined}
+          >
+            {item.title}
+            {isExternal && <ArrowUpRight className="h-4 w-4 text-muted-foreground" />}
+          </Anchor>
+        );
+        return isSheet ? (
+          <SheetClose key={item.title + item.href} asChild>
+            {Comp}
+          </SheetClose>
+        ) : (
+          Comp
+        );
+      })}
+    </>
+  );
+}
